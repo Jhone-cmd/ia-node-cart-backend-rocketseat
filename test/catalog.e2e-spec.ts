1988ba381b13d24c121acb9823417b60d09b83f1
jhone-cmd <jhone.1941@gmail.com>
2025-12-09 10:00:00 -0300

    the test for the catalog has been created.

--- test/catalog.e2e-spec.ts
+++ test/catalog.e2e-spec.ts
@@ -1,10 +1,10 @@
-import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
+import { Test, TestingModule } from '@nestjs/testing';
 import request from 'supertest';
 import { App } from 'supertest/types';
 import { AppModule } from './../src/app.module';
 
-describe('AppController (e2e)', () => {
+describe('CatalogController (e2e)', () => {
   let app: INestApplication<App>;
 
   beforeEach(async () => {
@@ -16,10 +16,10 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  it('/ (GET)', async () => {
+    const response = await request(app.getHttpServer()).get('/catalog');
+
+    expect(response.status).toBe(200);
+    expect(response.body).toHaveLength(36);
   });
 });
