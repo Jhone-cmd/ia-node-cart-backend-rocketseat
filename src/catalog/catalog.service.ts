439cd96f19641139102faea76b5aa365570a250f
jhone-cmd <jhone.1941@gmail.com>
2025-12-29 10:18:26 -0300

    add llm Service functionality to the catalog service; when the application starts, embed the products.

--- src/catalog/catalog.service.ts
+++ src/catalog/catalog.service.ts
@@ -1,4 +1,6 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnApplicationBootstrap } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { LlmService } from '../shared/llm.service';
 import { PostgresService } from '../shared/postgres.service';
 
 type Product = {
@@ -10,8 +12,28 @@ type Product = {
 };
 
 @Injectable()
-export class CatalogService {
-  constructor(private readonly postgresService: PostgresService) {}
+export class CatalogService implements OnApplicationBootstrap {
+  constructor(
+    private readonly postgresService: PostgresService,
+    private readonly llmService: LlmService,
+    private readonly configService: ConfigService
+  ) {}
+
+  async onApplicationBootstrap() {
+    if (this.configService.get<string>('NODE_ENV') === 'test') {
+      return;
+    }
+    const products = await this.postgresService.client.query<Product>(
+      'SELECT id, name FROM products WHERE embedding IS NULL'
+    );
+
+    if (products.rowCount === 0) {
+      console.log('No products to embed');
+      return;
+    }
+
+    await this.llmService.batchEmbedProducts(products.rows);
+  }
 
   async getCatalog(search = '') {
     let query = `SELECT products.id, products.name, products.price, products.embedding, json_build_object('id', stores.id, 'name', stores.name) as store FROM products JOIN stores ON products.store_id = stores.id`;
